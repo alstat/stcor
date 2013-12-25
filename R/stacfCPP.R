88e498ca557eac248ef8eb0e8370939450975be7
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-12-25 17:46:40 +0800

    Create stacfCPP.R

---
+++ R/stacfCPP.R
@@ -0,0 +1,16 @@
+stacfCPP <- function(data, 
+                      wmatrix = NULL,
+                      timelag = 1){
+  if(!is.data.frame(data))
+    stop("data should be data.frame class")
+  if(is.data.frame(data))
+    data = t(as.matrix(data))
+  if(is.null(wmatrix)){
+    out <- stacf1(x = data, timelag)
+    return(out / sqrt((sum((data)^2)) * (sum(data^2))))
+  }
+  if(!is.null(wmatrix)){
+    out <- stacf2(x = data, y = wmatrix, timelag)
+    return(out / sqrt((sum((wmatrix %*% data)^2)) * (sum(data^2))))
+  }
+}
