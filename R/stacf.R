11103d4b77f5ff09c74bf02c691305e32fb0b5b2
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-12-25 17:45:38 +0800

    Update stacf.R

--- R/stacf.R
+++ R/stacf.R
@@ -1,9 +1,9 @@
-stacf <- function(data, max.timelag, wmatrices = list(...)){
+stacfC <- function(data, max.timelag, wmatrices = list(...)){
   stacf.tab <- matrix(NA, nrow = max.timelag, ncol = length(wmatrices))
   
   for(j in 1:length(wmatrices)){
     for(i in 1:max.timelag){
-      stacf.tab[i, j] <- stacf.eq(data = data, wmatrix = wmatrices[[j]], timelag = i)
+      stacf.tab[i, j] <- stacfCPP(data = data, wmatrix = wmatrices[[j]], timelag = i)
     }
   }
   return(stacf.tab)
