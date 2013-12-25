fed00f62b34fe847c9c88f115ce9a8125ed71615
Al-Ahmadgaid Asaad <alstated@gmail.com>
2013-12-25 17:56:39 +0800

    Update and rename stacovf.R to stacovfCPP.R

---
+++ R/stacovfCPP.R
@@ -0,0 +1,24 @@
+stacovfCPP <- function(data, 
+                    wmatrix1 = NULL, 
+                    wmatrix2 = NULL, 
+                    timelag = 1){
+  a = dim(data)[1]
+  b = dim(data)[2]
+  sumn <- 0
+  if(timelag < 0){
+    timelag = -timelag
+  }
+  if(is.null(wmatrix1) && is.null(wmatrix2)) {
+    out <- stacovf1(data, timelag)
+  }
+  if(!is.null(wmatrix1) && is.null(wmatrix2)){
+    out <- stacovf2(data, timelag, wmatrix1)
+  }
+  if(is.null(wmatrix1) && !is.null(wmatrix2)) {
+    out <- stacovf3(data, timelag, wmatrix2)
+  }
+  if(!is.null(wmatrix1) && !is.null(wmatrix2)){
+    out <- stacovf4(data, timelag, wmatrix1, wmatrix2)
+  }
+  return(out)
+}
