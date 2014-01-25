11d06d64d8c716f3aecca1ffcf086ce8bc44bd9d
Al-Ahmadgaid Asaad <alstated@gmail.com>
2014-01-25 11:37:44 +0800

    Update stacfCPP.R

--- R/stacfCPP.R
+++ R/stacfCPP.R
@@ -1,10 +1,6 @@
 stacfCPP <- function(data, 
                      wmatrix = NULL,
                      timelag = 1){
-  if(!is.data.frame(data))
-    stop("data should be data.frame class")
-  if(is.data.frame(data))
-    data = t(as.matrix(data))
   if(is.null(wmatrix)){
     out <- stacf1(x = data, timelag)
     return(out / sqrt((sum((data)^2)) * (sum(data^2))))
